d0554b5b0bede4da8d38dc21879ba40d21aeb3cf
IvlevEvgeniy <79964221+IvlevEvgeniy@users.noreply.github.com>
2021-03-28 09:03:36 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <cstdlib>
+#define N 3
+using namespace std;
+
+void trans(double A[N][N], double B[N][N]) {
+	int i, j;
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			B[i][j] = A[j][i];
+}
+
+void trans(double A[N][N]) {
+	int i, j;
+	double s;
+	for (i = 0; i < N; i++)
+		for (j = i + 1; j < N; j++) {
+			s = A[i][j];
+			A[i][j] = A[j][i];
+			A[j][i] = s;
+		}
+}
+
+void fill(double A[N][N]) {
+	int i, j;
+	for (i = 0; i < N; i++)
+		for (j = 0; j < N; j++)
+			A[i][j] = rand() % 10;
+}
+
+void show(double A[N][N]) {
+	int i, j;
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++)
+			cout << A[i][j] << " ";
+		cout << endl;
+	}
+}
+int main() {
+	double A[N][N], B[N][N];
+	cout << "Initial matrix:\n";
+	fill(A);
+	show(A);
+	cout << "After transform:\n";
+	trans(A, B);
+	show(B);
+	return 0;
+}
